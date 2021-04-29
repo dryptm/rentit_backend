e905171f5d39881fa563d9c3963c7978a91ee6d9
Vinaya Kumar <vinay9415756155@gmail.com>
2021-04-30 01:33:25 +0530

    Add files via upload

--- file.js
+++ file.js
@@ -55,87 +55,89 @@ var arr = [];
     l = document.querySelectorAll(".SRCard").length;
 
 
-    // var bhk = document.querySelector(".m-srp-card__title__bhk").innerText;
-    // var location = document.querySelector(".m-srp-card__title").innerText;
+    var bhk = document.querySelector(".m-srp-card__title__bhk").innerText;
+   
+//location........................   
+    var location = document.querySelector(".m-srp-card__title").innerText;
 
 
-
-    // var c = document.querySelector(".m-srp-card__price").innerText;
-    // var res = c.split("           ");
-    // var str1 = res[5].split("");
-    // var arr = [];
-    // for (var i = 0; i < str1.length; i++) {
-    //   if (str1[i] !== "\t" && str1[i] !== " " && str1[i] !== "\n") {
-    //     arr.push(str1[i]);
-    //   }
-    // }
-    // var price = [];
-    // for (var i = 0; i < arr.length; i++) {
-    //   if (arr[i] !== ",") {
-    //     price.push(arr[i]);
-    //   }
-    // }
-    // price = Number(price.join(''))
-
-
-
-
-
-    // var sqrt = document.querySelector("#propertyArea52465999").value
-    // var sqrt_split = sqrt.split('')
-    // var arr_sqrt = []
-    // for (var i = 0; i < sqrt_split.length; i++) {
-    //   if (sqrt_split[i] !== "s" && sqrt_split[i] !== "q" && sqrt_split[i] !== "f" && sqrt_split[i] !== "t" && sqrt_split[i] !== " ") {
-    //     arr_sqrt.push(sqrt_split[i])
-    //   }
-    // }
-    // sqrt = Number(arr_sqrt.join(''))
-
-
-
-
-
-
-    for (var i = 0; i < l; i++) {
-      c = document.querySelectorAll(".SRCard")[i].innerText; //innertext
-      b = c.split("\n");
-      d = b[4];
-      d = d.substring(1);
-      d = d.replace(',', '');
-      d = d.replace(' ', '');
-      d = Number(d);
-      price = d;
-
-      dat = b[3];
-      dat = dat.replace("Posted:", '');
-      dat = dat.substring(1);
-
-      sq = b[6];
-      vr = sq.split(" ");
-      chk = vr.find(function (val) {
-        return val === "sqft"
-      })
-      square_ft = 0;
-      if (chk === "sqft") {
-        square_ft = Number(vr[vr.length - 2])
-      } else {
-        //wrong desc
+//price..............................
+    var c = document.querySelector(".m-srp-card__price").innerText;
+    var res = c.split("           ");
+    var str1 = res[5].split("");
+    var arr = [];
+    for (var i = 0; i < str1.length; i++) {
+      if (str1[i] !== "\t" && str1[i] !== " " && str1[i] !== "\n") {
+        arr.push(str1[i]);
       }
-
-
-      data_code = document.querySelectorAll(".SRCard")[i].dataset.code; //dataset.code
-      data_code = data_code.replace("openDetailPage(event, ", "");
-      data_code = data_code.replace(");", "");
-      data = data_code.split(',');
-      link = data[0];
-      link = link.replace("'", '');
-      link = link.replace("'", '');
-      //var sum_up={price:price, dat:dat, square_ft:square_ft, link:link}
-      var sum_up = [price, dat, square_ft, link]
-      arr.push(sum_up);
-
-      alert(arr[i])
     }
+    var price = [];
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] !== ",") {
+        price.push(arr[i]);
+      }
+    }
+    price = Number(price.join(''))
+
+
+
+
+//sqrt................................
+    var sqrt = document.querySelector("#propertyArea52465999").value
+    var sqrt_split = sqrt.split('')
+    var arr_sqrt = []
+    for (var i = 0; i < sqrt_split.length; i++) {
+      if (sqrt_split[i] !== "s" && sqrt_split[i] !== "q" && sqrt_split[i] !== "f" && sqrt_split[i] !== "t" && sqrt_split[i] !== " ") {
+        arr_sqrt.push(sqrt_split[i])
+      }
+    }
+    sqrt = Number(arr_sqrt.join(''))
+
+
+
+
+
+
+    // for (var i = 0; i < l; i++) {
+    //   c = document.querySelectorAll(".SRCard")[i].innerText; //innertext
+    //   b = c.split("\n");
+    //   d = b[4];
+    //   d = d.substring(1);
+    //   d = d.replace(',', '');
+    //   d = d.replace(' ', '');
+    //   d = Number(d);
+    //   price = d;
+
+    //   dat = b[3];
+    //   dat = dat.replace("Posted:", '');
+    //   dat = dat.substring(1);
+
+    //   sq = b[6];
+    //   vr = sq.split(" ");
+    //   chk = vr.find(function (val) {
+    //     return val === "sqft"
+    //   })
+    //   square_ft = 0;
+    //   if (chk === "sqft") {
+    //     square_ft = Number(vr[vr.length - 2])
+    //   } else {
+    //     //wrong desc
+    //   }
+
+
+    //   data_code = document.querySelectorAll(".SRCard")[i].dataset.code; //dataset.code
+    //   data_code = data_code.replace("openDetailPage(event, ", "");
+    //   data_code = data_code.replace(");", "");
+    //   data = data_code.split(',');
+    //   link = data[0];
+    //   link = link.replace("'", '');
+    //   link = link.replace("'", '');
+    //   //var sum_up={price:price, dat:dat, square_ft:square_ft, link:link}
+    //   var sum_up = [price, dat, square_ft, link]
+    //   arr.push(sum_up);
+
+    //   alert(arr[i])
+    // }
   })
 
 
