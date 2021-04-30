655c381db0400484608ba28a249317fca5d6bb58
Vinaya Kumar <vinay9415756155@gmail.com>
2021-04-30 17:12:10 +0530

    Add files via upload

--- file.js
+++ file.js
@@ -1,6 +1,5 @@
 const puppeteer = require('puppeteer');
 
-var arr = [];
 (async () => {
   const browser = await puppeteer.launch({
     headless: false,
@@ -17,6 +16,12 @@ var arr = [];
 
   var l;
   var link, data_code, data, square_ft, sq, vr, chk, c, b, d, price, dat;
+  
+  
+  
+  // var properties=[];
+
+
 
 
   const search_string = "lucknow rent 3bhk"
@@ -47,105 +52,69 @@ var arr = [];
 
 
 
-  console.log(arr);
   await page.evaluate(() => {
 
-    console.log(arr);
-
-    l = document.querySelectorAll(".SRCard").length;
+    var numberofblocks = document.querySelectorAll(".m-srp-card").length;
 
 
-    var bhk = document.querySelector(".m-srp-card__title__bhk").innerText;
-   
-//location........................   
-    var location = document.querySelector(".m-srp-card__title").innerText;
+    for (var i = 0; i < numberofblocks; i++) {
+      // bhk
+      var bhk = document.querySelectorAll(".m-srp-card__title__bhk")[i].innerText;
+      var arr_bhk = bhk.split("");
+      var arr1_bhk = arr_bhk[0];
+      bhk = arr1_bhk;
+      bhk = Number(bhk);
 
 
-//price..............................
-    var c = document.querySelector(".m-srp-card__price").innerText;
-    var res = c.split("           ");
-    var str1 = res[5].split("");
-    var arr = [];
-    for (var i = 0; i < str1.length; i++) {
-      if (str1[i] !== "\t" && str1[i] !== " " && str1[i] !== "\n") {
-        arr.push(str1[i]);
+
+
+      //location........................   
+      var location_prop = document.querySelectorAll(".m-srp-card__title")[i].innerText;
+
+
+      //price..............................
+      var c = document.querySelectorAll(".m-srp-card__price")[i].innerHTML;
+      var res = c.split("           ");
+      var str1 = res[5].split("");
+      var arr = [];
+      for (var j = 0; j < str1.length; j++) {
+        if (str1[j] !== "\t" && str1[j] !== " " && str1[j] !== "\n") {
+          arr.push(str1[j]);
+        }
       }
-    }
-    var price = [];
-    for (var i = 0; i < arr.length; i++) {
-      if (arr[i] !== ",") {
-        price.push(arr[i]);
+      var price = [];
+      for (var j = 0; j < arr.length; j++) {
+        if (arr[j] !== ",") {
+          price.push(arr[j]);
+        }
       }
+      price = Number(price.join(''));
+
+
+
+      //sqrt................................
+      var sqrt = document.querySelectorAll(".m-srp-card__title")[i].innerText;
+
+
+      sqrt = Number(sqrt.split(" ")[(sqrt.split(" ").length) - 2])
+
+
+
+
+      var object_property = {
+        "bhk": bhk,
+        "location": location_prop,
+        "price": price,
+        "sqrt": sqrt
+      };
+      // properties.push(object_property);
+      alert(object_property.location)
     }
-    price = Number(price.join(''))
 
 
-
-
-//sqrt................................
-    var sqrt = document.querySelector("#propertyArea52465999").value
-    var sqrt_split = sqrt.split('')
-    var arr_sqrt = []
-    for (var i = 0; i < sqrt_split.length; i++) {
-      if (sqrt_split[i] !== "s" && sqrt_split[i] !== "q" && sqrt_split[i] !== "f" && sqrt_split[i] !== "t" && sqrt_split[i] !== " ") {
-        arr_sqrt.push(sqrt_split[i])
-      }
-    }
-    sqrt = Number(arr_sqrt.join(''))
-
-
-
-
-
-
-    // for (var i = 0; i < l; i++) {
-    //   c = document.querySelectorAll(".SRCard")[i].innerText; //innertext
-    //   b = c.split("\n");
-    //   d = b[4];
-    //   d = d.substring(1);
-    //   d = d.replace(',', '');
-    //   d = d.replace(' ', '');
-    //   d = Number(d);
-    //   price = d;
-
-    //   dat = b[3];
-    //   dat = dat.replace("Posted:", '');
-    //   dat = dat.substring(1);
-
-    //   sq = b[6];
-    //   vr = sq.split(" ");
-    //   chk = vr.find(function (val) {
-    //     return val === "sqft"
-    //   })
-    //   square_ft = 0;
-    //   if (chk === "sqft") {
-    //     square_ft = Number(vr[vr.length - 2])
-    //   } else {
-    //     //wrong desc
-    //   }
-
-
-    //   data_code = document.querySelectorAll(".SRCard")[i].dataset.code; //dataset.code
-    //   data_code = data_code.replace("openDetailPage(event, ", "");
-    //   data_code = data_code.replace(");", "");
-    //   data = data_code.split(',');
-    //   link = data[0];
-    //   link = link.replace("'", '');
-    //   link = link.replace("'", '');
-    //   //var sum_up={price:price, dat:dat, square_ft:square_ft, link:link}
-    //   var sum_up = [price, dat, square_ft, link]
-    //   arr.push(sum_up);
-
-    //   alert(arr[i])
-    // }
   })
 
 
-  alert(arr[0].price)
-
-  // for (var i = 0; i < l; i++) {
-  console.log(arr[0])
-  // }
 
 
 
