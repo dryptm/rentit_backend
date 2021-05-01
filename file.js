350d087e6cb744215567bbb8dfd3e3472f58a941
Vinaya Kumar <vinay9415756155@gmail.com>
2021-05-02 02:08:52 +0530

    Add files via upload

--- file.js
+++ file.js
@@ -24,7 +24,7 @@ const puppeteer = require('puppeteer');
 
 
 
-  const search_string = "lucknow rent 3bhk"
+  const search_string = "lucknow rent 5bhk"
 
   console.log("-x-x-x-x- start -x-x-x-x-x-x-")
   await page.goto("https://www.google.com/search?q=" + search_string);
@@ -42,7 +42,33 @@ const puppeteer = require('puppeteer');
     waitUntil: 'networkidle0',
   });
 
-  console.log
+
+
+  const page1 = await browser.newPage();
+  page1.setViewport({
+    width: 0,
+    height: 0
+  });
+
+  const search_string1 = "lucknow rent 3bhk"
+
+  console.log("-x-x-x-x- start1 -x-x-x-x-x-x-")
+  await page1.goto("https://www.google.com/search?q=" + search_string1);
+  console.log("x-x-x-x-x- next1 -x-x-x-x-x")
+  await page1.evaluate(() => {
+    document.querySelector('.DKV0Md').click()
+
+  })
+
+
+
+
+  // full page load wait
+  await page1.waitForNavigation({
+    waitUntil: 'networkidle0',
+  });
+
+
 
   var page_prop = await page.evaluate(() => {
 
@@ -75,31 +101,50 @@ const puppeteer = require('puppeteer');
 
 
       //price..............................
-      var c = document.querySelectorAll(".m-srp-card__price")[(i * 2) + 1].innerHTML;
-      var res = c.split("           ");
-      var str1 = res[5].split("");
-      var arr = [];
-      for (var j = 0; j < str1.length; j++) {
-        if (str1[j] !== "\t" && str1[j] !== " " && str1[j] !== "\n") {
-          arr.push(str1[j]);
-        }
-      }
+      var c = document.querySelectorAll(".m-srp-card__info")[i].children[1].firstChild.data;
+
+
       var price = [];
-      for (var j = 0; j < arr.length; j++) {
-        if (arr[j] !== ",") {
-          price.push(arr[j]);
+
+      if (c.length > 10) {
+        var res = c.split("           ");
+        var str1 = res[5].split("");
+        var arr = [];
+        for (var j = 0; j < str1.length; j++) {
+          if (str1[j] !== "\t" && str1[j] !== " " && str1[j] !== "\n") {
+            arr.push(str1[j]);
+          }
         }
+        var lack_check = arr[(arr.length) - 3] + arr[(arr.length) - 2] + arr[(arr.length) - 1]
+
+        if (lack_check === "Lac") {
+          for (var j = 0; j < ((arr.length) - 3); j++) {
+            price.push(arr[j]);
+          }
+          price = (Number(price.join('')) * 100000);
+        } else {
+          for (var j = 0; j < arr.length; j++) {
+            if (arr[j] !== ",") {
+              price.push(arr[j]);
+            }
+          }
+          price = Number(price.join(''));
+        }
+      } else {
+        price = "Call for price";
       }
-      price = Number(price.join(''));
 
 
 
-      //sqrt................................
-      var sqft = document.querySelectorAll(".m-srp-card__title")[i].innerText;
+      //size................................
+      var size = document.querySelectorAll(".m-srp-card__title")[i].innerText;
 
+      if (Number(size.split(" ")[(size.split(" ").length) - 2]) % 1 == 0) {
+        size = (size.split(" ")[(size.split(" ").length) - 2] + size.split(" ")[(size.split(" ").length) - 1]);
 
-      sqft = Number(sqft.split(" ")[(sqft.split(" ").length) - 2])
-
+      } else {
+        size = "unknown"
+      }
 
 
 
@@ -109,7 +154,7 @@ const puppeteer = require('puppeteer');
         "bhk": bhk,
         "location": location_prop,
         "price": price,
-        "sqft": sqft
+        "size": size
       };
       if (document.querySelectorAll(".m-photo__fig")[i].firstElementChild && document.querySelectorAll(".m-photo__fig")[i].firstElementChild.dataset.src) {
         object_property.picture = document.querySelectorAll(".m-photo__fig")[i].firstElementChild.dataset.src;
@@ -117,13 +162,119 @@ const puppeteer = require('puppeteer');
 
       property_details.push(object_property);
     }
-    
+
     return property_details;
 
   })
-  
 
   console.log(page_prop);
+
+
+
+
+
+
+
+  var page_prop1 = await page1.evaluate(() => {
+
+    var numberofblocks = document.querySelectorAll(".m-srp-card").length;
+    var property_details = [];
+
+    for (let i = 0; i < numberofblocks; i++) {
+
+      //redirect_url....................................
+      var redirect_url = document.querySelectorAll(".SRCard")[i].dataset.code;
+      var arr_urlx = redirect_url.split(',')
+      var arr_urly = arr_urlx[1].split("'")
+      redirect_url = arr_urly[1];
+
+
+
+
+      // bhk..................................
+      var bhk = document.querySelectorAll(".m-srp-card__title__bhk")[i].innerText;
+      var arr_bhk = bhk.split("");
+      var arr1_bhk = arr_bhk[0];
+      bhk = arr1_bhk;
+      bhk = Number(bhk);
+
+
+
+
+      //location........................   
+      var location_prop = document.querySelectorAll(".m-srp-card__title")[i].innerText;
+
+
+      //price..............................
+      var c = document.querySelectorAll(".m-srp-card__info")[i].children[1].firstChild.data;
+
+
+      var price = [];
+
+      if (c.length > 10) {
+        var res = c.split("           ");
+        var str1 = res[5].split("");
+        var arr = [];
+        for (var j = 0; j < str1.length; j++) {
+          if (str1[j] !== "\t" && str1[j] !== " " && str1[j] !== "\n") {
+            arr.push(str1[j]);
+          }
+        }
+        var lack_check = arr[(arr.length) - 3] + arr[(arr.length) - 2] + arr[(arr.length) - 1]
+
+        if (lack_check === "Lac") {
+          for (var j = 0; j < ((arr.length) - 3); j++) {
+            price.push(arr[j]);
+          }
+          price = (Number(price.join('')) * 100000);
+        } else {
+          for (var j = 0; j < arr.length; j++) {
+            if (arr[j] !== ",") {
+              price.push(arr[j]);
+            }
+          }
+          price = Number(price.join(''));
+        }
+      } else {
+        price = "Call for price";
+      }
+
+
+
+      //size................................
+      var size = document.querySelectorAll(".m-srp-card__title")[i].innerText;
+
+      if (Number(size.split(" ")[(size.split(" ").length) - 2]) % 1 == 0) {
+        size = (size.split(" ")[(size.split(" ").length) - 2] + size.split(" ")[(size.split(" ").length) - 1]);
+
+      } else {
+        size = "unknown"
+      }
+
+
+
+      var object_property = {
+
+        "redirect_url": redirect_url,
+        "bhk": bhk,
+        "location": location_prop,
+        "price": price,
+        "size": size
+      };
+      if (document.querySelectorAll(".m-photo__fig")[i].firstElementChild && document.querySelectorAll(".m-photo__fig")[i].firstElementChild.dataset.src) {
+        object_property.picture = document.querySelectorAll(".m-photo__fig")[i].firstElementChild.dataset.src;
+      }
+
+      property_details.push(object_property);
+    }
+
+    return property_details;
+
+  })
+  console.log(page_prop1)
+
+
+
   await browser.close();
   app.get("/", (req, res) => {
     res.send(page_prop)
